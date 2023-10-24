5e6fa5fbb4af80be57530fa930c02b4e66e147a8
JustScorpio <skorpiello@gmail.com>
2023-10-24 23:31:26 +0300

    Add react-router-dom

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,12 @@ import { Component, ReactNode, createContext } from "react";
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Routes
+} from 'react-router-dom';
 
 interface RequestState { url: string, output: string };
 
@@ -21,23 +27,51 @@ export default class App extends Component<{}, RequestState>{
 
   setResponse = (url: string) => {
     axios.get(url).then((response) => {
-      this.setState({ output: JSON.stringify(response.data)});
+      this.setState({ output: JSON.stringify(response.data) });
     });
   }
 
   render(): ReactNode {
     return <>
-      <RequestContext.Provider value={this.state}>
-        <InputUrl handler = {this.setUrl}/>
-        <Button handler = {this.setResponse}/>
-        <OutputText />
-      </RequestContext.Provider>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/Register'}>Register</Link>
+            </li>
+            <li>
+              <Link to={'/Login'}>Login</Link>
+            </li>
+            <li>
+              <Link to={'/randomtext'}>This page is not exists</Link>
+            </li>
+            <li>
+              <Link to={'/whatareyoulookingfor'}>This page is not exists too</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={
+            <RequestContext.Provider value={this.state}>
+              <InputUrl handler={this.setUrl} />
+              <Button handler={this.setResponse} />
+              <OutputText />
+            </RequestContext.Provider>
+          } />
+          <Route path="Login" element={<div>Hey! Login!</div>}/>
+          <Route path="Register" element={<div>Hey! Register!</div>}/>
+          <Route path="*" element={<div>404</div>}/>
+        </Routes>
+      </BrowserRouter>
     </>;
   }
 }
 
 //Компонент-строка ввода (класс)
-class InputUrl extends Component<{handler: {(urlToCall: string): void}}, RequestState>{
+class InputUrl extends Component<{ handler: { (urlToCall: string): void } }, RequestState>{
 
   render(): ReactNode {
     return <input type="text" onChange={evt => this.props.handler(evt.target.value)} />
@@ -45,16 +79,16 @@ class InputUrl extends Component<{handler: {(urlToCall: string): void}}, Request
 }
 
 //Компонент-кнопка (функция)
-interface InputUrlProps {handler: {(urlToCall: string): void}}
+interface InputUrlProps { handler: { (urlToCall: string): void } }
 
 function Button(props: InputUrlProps) {
-    return <RequestContext.Consumer>
-      {context =>
-        <button onClick={() => props.handler(context.url)}>
-          Отправить
-        </button>
-      }
-    </RequestContext.Consumer>
+  return <RequestContext.Consumer>
+    {context =>
+      <button onClick={() => props.handler(context.url)}>
+        Отправить
+      </button>
+    }
+  </RequestContext.Consumer>
 }
 
 //Компонент-ответ на запрос (класс)
